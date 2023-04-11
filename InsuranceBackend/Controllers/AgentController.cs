93d42a9436797dc917c84461537352b8619a28bd
Judes0n <judesonjoy111@gmail.com>
2023-04-11 20:57:22 +0530

    Agent Controller Modified

--- InsuranceBackend/Controllers/AgentController.cs
+++ InsuranceBackend/Controllers/AgentController.cs
@@ -3,6 +3,7 @@ using InsuranceBackend.Services;
 using InsuranceBackend.Enum;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Insurance.Controllers
 {
@@ -12,12 +13,49 @@ namespace Insurance.Controllers
     {
         AgentService _agentServices;
         UserService _userService;
+        InsuranceDbContext _dbContext;
 
         public AgentController()
         {
             _agentServices = new AgentService();
             _userService = new UserService();
+            _dbContext = new InsuranceDbContext();
         }
 
+        [HttpGet]
+        [Route("GetPolicyTerms")]
+
+        public IActionResult GetTerms(int policytermId)
+        {
+            var res = _dbContext.PolicyTerms.FirstOrDefault(pt => pt.PolicyTermId == policytermId);
+            return Ok(res);
+        }
+
+        [HttpGet]
+        [Route("GetClientPolicies")]
+
+        public IActionResult GetClientPolicies(int agentId)
+        {
+            var res = _dbContext.ClientPolicies.Where(cp=>cp.AgentId == agentId).ToList();
+            return Ok(res);
+        }
+
+        [HttpGet]
+        [Route("GetAgent")]
+
+        public IActionResult GetAgent(int userId)
+        {
+            var res = _dbContext.Agents.FirstOrDefault(a=>a.UserId == userId);
+            return Ok(res);
+        }
+
+        [HttpGet]
+        [Route("GetPolicy")]
+
+        public IActionResult GetPolicy(int policyId) 
+        { 
+            var res = _dbContext.Policies.FirstOrDefault(p=>p.PolicyId == policyId);
+            return Ok(res);
+        }
     }
 }
--- InsuranceBackend/Controllers/ClientController.cs
+++ InsuranceBackend/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using InsuranceBackend.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InsuranceBackend.Controllers
@@ -7,5 +8,24 @@ namespace InsuranceBackend.Controllers
     [ApiController]
     public class ClientController : ControllerBase
     {
+        ClientServices _clientService;
+        UserService _userService;
+
+        public ClientController()
+        {
+            _clientService = new ClientServices();
+            _userService = new UserService();
+        }
+
+        [HttpGet]
+        [Route("GetClient")]
+
+        public IActionResult GetClientById(int clientId)
+        {
+           var dbclient  = _clientService.GetClient(clientId);
+            return Ok(dbclient);
+        }
+
+
     }
 }
